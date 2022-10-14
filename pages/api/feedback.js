c101fa90f9ee7ddeea1323a7b3c787f5c7e000e5
Logan Thomas <logan.thomas@forsta.com>
2022-10-14 00:49:07 -0600

    added getStaticProps to grab data

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,4 +1,39 @@
+import fs from "fs";
+import path from "path";
+
+export const buildFeedbackPath = () => {
+  return path.join(process.cwd(), "data", "feedback.json");
+};
+
+export const extractFeedback = (filePath) => {
+  const fileData = fs.readFileSync(buildFeedbackPath());
+  const data = JSON.parse(fileData);
+  return data;
+};
+
 function handler(req, res) {
-  res.status(200).json({ message: "This works" });
+  const routes = {
+    GET: () => {
+      const data = extractFeedback(buildFeedbackPath());
+      res.status(201).json({ message: "Success!", feedback: data });
+    },
+    POST: () => {
+      const { email, text } = req.body;
+
+      const newFeedback = {
+        id: new Date().toISOString(),
+        email,
+        text,
+      };
+
+      const data = extractFeedback(buildFeedbackPath());
+      data.push(newFeedback);
+      fs.writeFileSync(filePath, JSON.stringify(data));
+      return res
+        .status(201)
+        .json({ message: "Success!", feedback: newFeedback });
+    },
+  };
+  routes[req.method]();
 }
 export default handler;
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,72 @@
-import { Fragment } from 'react';
-import Head from 'next/head';
+import { Fragment, useRef, useState } from "react";
+import Head from "next/head";
 
-import FeaturedPosts from '../components/home-page/featured-posts';
-import Hero from '../components/home-page/hero';
-import { getFeaturedPosts } from '../lib/posts-util';
+import FeaturedPosts from "../components/home-page/featured-posts";
+import Hero from "../components/home-page/hero";
+import { getFeaturedPosts } from "../lib/posts-util";
 
 function HomePage(props) {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    const body = { email: enteredEmail, text: enteredFeedback };
+
+    console.log(body);
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
+  const loadFeedbackHandler = () => {
+    fetch("/api/feedback")
+      .then((response) => response.json())
+      .then(({ feedback }) => setFeedbackItems(feedback));
+  };
+
   return (
     <Fragment>
       <Head>
         <title>Max' Blog</title>
         <meta
-          name='description'
-          content='I post about programming and web development.'
+          name="description"
+          content="I post about programming and web development."
         />
       </Head>
-      <Hero />
-      <FeaturedPosts posts={props.posts} />
+      <div>
+        <form onSubmit={submitFormHandler}>
+          <div>
+            <label htmlFor="email">Your Email Address</label>
+            <input type="email" id="email" ref={emailInputRef} />
+          </div>
+          <div>
+            <label htmlFor="feedback">Your Feedback</label>
+            <textarea rows="5" id="feedback" ref={feedbackInputRef}></textarea>
+          </div>
+          <button>Send Feedback</button>
+        </form>
+        <hr />
+        <button onClick={loadFeedbackHandler}>Load Feedback</button>
+        <ul>
+          {feedbackItems.map((item) => (
+            <li>item</li>
+          ))}
+        </ul>
+      </div>
     </Fragment>
   );
 }
